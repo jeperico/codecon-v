c94f4de05e37c95409370b663d86812e62b579b0
jeperico <joaoeperico@gmail.com>
2026-07-30 14:50:24 -0300

    feat: create reservation services

--- v/src/main/java/com/codeconv/domain/service/ReservationService.java
+++ v/src/main/java/com/codeconv/domain/service/ReservationService.java
@@ -4,8 +4,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+import com.codeconv.common.exception.ResourceNotFoundException;
 import com.codeconv.domain.dto.CreateReservationRequest;
 import com.codeconv.domain.dto.ReservationResponse;
+import com.codeconv.domain.entity.Batch;
+import com.codeconv.domain.entity.Reservation;
+import com.codeconv.domain.repository.BatchRepository;
 import com.codeconv.domain.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -14,12 +18,25 @@ import lombok.RequiredArgsConstructor;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final BatchRepository batchRepository;
 
     public ReservationResponse createReservation(CreateReservationRequest request) {
-        return ReservationResponse.builder()
-                .id(UUID.randomUUID())
+        Batch batch = batchRepository
+                .findById(UUID.fromString("11111111-1111-1111-1111-111111111111"))
+                .orElseThrow(() -> new ResourceNotFoundException("Wedding not found"));
+
+        Reservation reservation = Reservation.builder()
                 .cpf(request.getCpf())
                 .quantity(request.getQuantity())
+                .status("SOLD")
+                .batch(batch)
+                .build();
+
+        reservationRepository.save(reservation);
+
+        return ReservationResponse.builder()
+                .cpf(request.getCpf())
+                .quantity(reservation.getQuantity())
                 .status("SOLD")
                 .build();
     }
